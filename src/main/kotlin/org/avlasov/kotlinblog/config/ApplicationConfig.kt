d7379396aceee4fd449deaacc908607540b32902
Vlasov Artem <avlasov@selectica.com>
2018-10-25 07:44:43 +0300

    Add base application config

---
+++ src/main/kotlin/org/avlasov/kotlinblog/config/ApplicationConfig.kt
@@ -0,0 +1,11 @@
+package org.avlasov.kotlinblog.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ *   Created By artemvlasov on 2018-10-24
+ **/
+@Configuration
+@ComponentScan(basePackages = ["org.avlasov.kotlinblog"])
+class ApplicationConfig
