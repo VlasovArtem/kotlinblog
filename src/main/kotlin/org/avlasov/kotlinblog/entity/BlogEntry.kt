7d1d27c55e9d325e6477ccdf17b01a80f30d5e5c
Vlasov Artem <avlasov@selectica.com>
2018-10-25 06:58:55 +0300

    Add Blog entry entity

---
+++ src/main/kotlin/org/avlasov/kotlinblog/entity/BlogEntry.kt
@@ -0,0 +1,36 @@
+package org.avlasov.kotlinblog.entity
+
+import javax.persistence.Entity
+
+/**
+ *   Created By artemvlasov on 2018-10-24
+ **/
+@Entity
+data class BlogEntry(val id: Long,
+                     var title: String,
+                     var description: String,
+                     var category: String,
+                     var image: ByteArray ) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is BlogEntry) return false
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (description != other.description) return false
+        if (category != other.category) return false
+        if (!image.contentEquals(other.image)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + category.hashCode()
+        result = 31 * result + image.contentHashCode()
+        return result
+    }
+}
