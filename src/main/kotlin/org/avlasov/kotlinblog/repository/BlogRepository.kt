58db665199bb83a112129c4878209ff49d650225
Vlasov Artem <avlasov@selectica.com>
2018-10-25 21:39:04 +0300

    Update blog entry repository with new string type

--- src/main/kotlin/org/avlasov/kotlinblog/repository/BlogRepository.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/repository/BlogRepository.kt
@@ -1,7 +1,6 @@
 package org.avlasov.kotlinblog.repository
 
 import org.avlasov.kotlinblog.entity.BlogEntry
-import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
@@ -9,4 +8,4 @@ import org.springframework.stereotype.Repository
  *   Created By artemvlasov on 2018-10-25
  **/
 @Repository
-interface BlogEntryRepository : MongoRepository<BlogEntry, ObjectId>
+interface BlogRepository : MongoRepository<BlogEntry, String>
