b5d6ad86e0659042e082a2f514e4099036a528a2
Vlasov Artem <avlasov@selectica.com>
2018-10-25 20:18:09 +0300

    Update Application class, Resolve issue with custom convertors

--- src/main/kotlin/org/avlasov/kotlinblog/Application.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/Application.kt
@@ -1,9 +1,6 @@
 package org.avlasov.kotlinblog
 
 import org.avlasov.kotlinblog.config.ApplicationConfig
-import org.avlasov.kotlinblog.entity.BlogEntry
-import org.avlasov.kotlinblog.repository.BlogEntryRepository
-import org.bson.types.ObjectId
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 
@@ -14,10 +11,5 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 class Application
 
 fun main(args: Array<String>) {
-    val run = SpringApplicationBuilder(ApplicationConfig::class.java).profiles("development").run(*args)
-    val blogEntryRepository = run.getBean(BlogEntryRepository::class.java)
-    val objectId = ObjectId.get()
-    blogEntryRepository.insert(BlogEntry(objectId, "test title", "test description", "category", "test".toByteArray()))
-    println(blogEntryRepository.findById(objectId))
-    run.close()
+    SpringApplicationBuilder(ApplicationConfig::class.java).profiles("development").run(*args)
 }
--- src/main/kotlin/org/avlasov/kotlinblog/config/database/MongoDBConfiguration.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/config/database/MongoDBConfiguration.kt
@@ -2,11 +2,18 @@ package org.avlasov.kotlinblog.config.database
 
 import com.mongodb.MongoClient
 import com.mongodb.MongoClientURI
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
+import org.springframework.data.mongodb.core.MongoTemplate
 
 @Profile("development")
 class MongoDBConfiguration : AbstractMongoDBConfiguration() {
 
+    override fun mongoTemplate(): MongoTemplate {
+        return MongoTemplate(mongoClient(), dbName)
+    }
+
+    @Bean
     override fun mongoClient(): MongoClient {
         return MongoClient(MongoClientURI("mongodb://localhost"))
     }
