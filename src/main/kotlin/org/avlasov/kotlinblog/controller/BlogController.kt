c1dfbe7d9e4c933703dba184895e3da1da05c62a
Vlasov Artem <avlasov@selectica.com>
2018-10-26 06:50:18 +0300

    Update controller methods with more precise request mapping annotations

--- src/main/kotlin/org/avlasov/kotlinblog/controller/BlogController.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/controller/BlogController.kt
@@ -5,28 +5,25 @@ import org.avlasov.kotlinblog.service.BlogService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
  *   Created By artemvlasov on 2018-10-25
  **/
 @RestController
-@RequestMapping("/blog")
+@RequestMapping("/api/blog")
 class BlogController(@Autowired private val blogService: BlogService) {
 
-    @RequestMapping(path = ["/add"], method = [RequestMethod.POST], consumes = [APPLICATION_JSON_VALUE])
+    @PostMapping(path = ["/add"], consumes = [APPLICATION_JSON_VALUE])
     fun add(blogControllerEntity: BlogControllerEntity): ResponseEntity<String> {
         blogService.add(blogControllerEntity)
         return ResponseEntity.ok("Blog entry successfully added")
     }
 
-    @RequestMapping(path = ["/get/all"], method = [RequestMethod.GET], produces = [APPLICATION_JSON_VALUE])
+    @GetMapping(path = ["/get/all"], produces = [APPLICATION_JSON_VALUE])
     fun getAll(): ResponseEntity<List<BlogEntry>> = ResponseEntity.ok(blogService.getAll())
 
-    @RequestMapping(path = ["/get"], method = [RequestMethod.GET], produces = [APPLICATION_JSON_VALUE])
+    @GetMapping(path = ["/get"], produces = [APPLICATION_JSON_VALUE])
     fun get(@RequestParam id: String): ResponseEntity<*> =
         blogService.get(id)
             .map { ResponseEntity.ok(it) }
