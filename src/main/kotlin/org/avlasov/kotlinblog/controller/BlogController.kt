2f258d88c4ad57ac64c9f4c96eef6ff1619420d0
Vlasov Artem <avlasov@selectica.com>
2018-11-09 09:32:29 +0200

    Update controllers
    Add user controller, update blog controller

--- src/main/kotlin/org/avlasov/kotlinblog/controller/BlogController.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/controller/BlogController.kt
@@ -3,6 +3,7 @@ package org.avlasov.kotlinblog.controller
 import org.avlasov.kotlinblog.entity.BlogEntry
 import org.avlasov.kotlinblog.service.BlogService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,27 +12,26 @@ import org.springframework.web.bind.annotation.*
  *   Created By artemvlasov on 2018-10-25
  **/
 @RestController
-@RequestMapping("/api/blog")
+@RequestMapping(path = ["/api/blog"], produces = [APPLICATION_JSON_VALUE], consumes = [APPLICATION_JSON_VALUE])
+@Profile("rest")
 class BlogController(@Autowired private val blogService: BlogService) {
 
-    @PostMapping(path = ["/add"], consumes = [APPLICATION_JSON_VALUE])
-    fun add(blogControllerEntity: BlogControllerEntity): ResponseEntity<String> {
-        blogService.add(blogControllerEntity)
+    @PostMapping(path = ["/add"])
+    fun add(blogEntry: BlogEntry): ResponseEntity<String> {
+        blogService.add(blogEntry)
         return ResponseEntity.ok("Blog entry successfully added")
     }
 
-    @GetMapping(path = ["/get/all"], produces = [APPLICATION_JSON_VALUE])
+    @GetMapping(path = ["/get/all"])
     fun getAll(): ResponseEntity<List<BlogEntry>> = ResponseEntity.ok(blogService.getAll())
 
-    @GetMapping(path = ["/get"], produces = [APPLICATION_JSON_VALUE])
+    @GetMapping(path = ["/get"])
     fun get(@RequestParam id: String): ResponseEntity<*> =
         blogService.get(id)
             .map { ResponseEntity.ok(it) }
             .orElseGet { ResponseEntity.notFound().build() }
 
-    data class BlogControllerEntity(val title: String,
-                                    val description: String,
-                                    val category: String,
-                                    val image: ByteArray)
+    @GetMapping(path = ["/get/user/"])
+    fun getUsersBlogEntries(@RequestParam userId: String) = blogService.getBlogEntriesByUserId(userId)
 
 }
---
+++ src/main/kotlin/org/avlasov/kotlinblog/controller/UserController.kt
@@ -0,0 +1,32 @@
+package org.avlasov.kotlinblog.controller
+
+import org.avlasov.kotlinblog.entity.User
+import org.avlasov.kotlinblog.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ *   Created By artemvlasov on 2018-11-09
+ **/
+@RestController
+@RequestMapping(path = ["/api/user"], consumes = [MediaType.APPLICATION_JSON_VALUE])
+@Profile("rest")
+class UserController(@Autowired private val userService: UserService) {
+
+
+    @GetMapping(path = ["/get"])
+    fun get(@RequestParam id: String): ResponseEntity<User> {
+        val user = userService.get(id)
+        if (user.isPresent) {
+            return ResponseEntity.ok(user.get())
+        }
+        return ResponseEntity.notFound().build()
+    }
+
+}
