6c3d529bea7893c090900afcf3780f6669524603
Vlasov Artem <avlasov@selectica.com>
2018-11-09 09:29:43 +0200

    Update repositories
    Add method to BlogRepository to find blog entries by user id. Add User repository

--- src/main/kotlin/org/avlasov/kotlinblog/repository/BlogRepository.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/repository/BlogRepository.kt
@@ -3,9 +3,14 @@ package org.avlasov.kotlinblog.repository
 import org.avlasov.kotlinblog.entity.BlogEntry
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 /**
  *   Created By artemvlasov on 2018-10-25
  **/
 @Repository
-interface BlogRepository : MongoRepository<BlogEntry, String>
+interface BlogRepository : MongoRepository<BlogEntry, String> {
+
+    fun findBlogEntriesByUserId(id: String): List<BlogEntry>
+
+}
