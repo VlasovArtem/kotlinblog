d97845c24288c73b0c04704b56f7c500e3223360
Vlasov Artem <avlasov@selectica.com>
2018-11-11 08:50:05 +0200

    Add root GraphQL Query and Mutation classes

---
+++ src/main/kotlin/org/avlasov/kotlinblog/graphql/mutation/Mutation.kt
@@ -0,0 +1,8 @@
+package org.avlasov.kotlinblog.graphql.mutation
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver
+
+/**
+ *   Created By artemvlasov on 2018-11-10
+ **/
+class Mutation : GraphQLMutationResolver
---
+++ src/main/kotlin/org/avlasov/kotlinblog/graphql/query/Query.kt
@@ -0,0 +1,8 @@
+package org.avlasov.kotlinblog.graphql.query
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver
+
+/**
+ *   Created By artemvlasov on 2018-11-10
+ **/
+class Query : GraphQLQueryResolver
