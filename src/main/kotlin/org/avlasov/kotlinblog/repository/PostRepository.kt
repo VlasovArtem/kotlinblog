4cb95cb27cb8263aaa6dc50ca2ae25e6e330d66e
Vlasov Artem <avlasov@selectica.com>
2018-11-11 08:35:42 +0200

    Update annotations for entities repositories

--- src/main/kotlin/org/avlasov/kotlinblog/repository/PostRepository.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/repository/PostRepository.kt
@@ -2,14 +2,15 @@ package org.avlasov.kotlinblog.repository
 
 import org.avlasov.kotlinblog.entity.Post
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 /**
  *   Created By artemvlasov on 2018-10-25
  **/
-@Repository
 interface PostRepository : MongoRepository<Post, String> {
 
     fun findPostsByAuthorId(id: String): List<Post>
+    fun findPostsByCreatedDateBetween(start: LocalDateTime, end: LocalDateTime): List<Post>
+    fun countPostByCreatedDateBetween(start: LocalDateTime, end: LocalDateTime): Long
 
 }
--- src/main/kotlin/org/avlasov/kotlinblog/repository/UserRepository.kt
+++ src/main/kotlin/org/avlasov/kotlinblog/repository/UserRepository.kt
@@ -2,10 +2,8 @@ package org.avlasov.kotlinblog.repository
 
 import org.avlasov.kotlinblog.entity.User
 import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
 
 /**
  *   Created By artemvlasov on 2018-11-09
  **/
-@Repository
 interface UserRepository : MongoRepository<User, String>
