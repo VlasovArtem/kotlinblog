6aab15cac881b96dc1f50ad748f0528817b78388
Vlasov Artem <avlasov@selectica.com>
2018-11-11 08:33:15 +0200

    Add Base abstract entity

---
+++ src/main/kotlin/org/avlasov/kotlinblog/entity/Base.kt
@@ -0,0 +1,8 @@
+package org.avlasov.kotlinblog.entity
+
+import org.springframework.data.annotation.Id
+
+/**
+ *   Created By artemvlasov on 2018-11-10
+ **/
+abstract class Base(@Id var id: String?)
